09b60f42633d29b7e93a90f7b4a1c7247c7172cf
tankpillow <tankpillow@gmail.com>
2025-07-29 13:29:37 -0400

    And so it begins

---
+++ src/main/java/dev/tankpillow/cosmicraft/Cosmicraft.java
@@ -0,0 +1,17 @@
+package dev.tankpillow.cosmicraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cosmicraft implements ModInitializer {
+	
+	public static final String MOD_ID = "cosmicraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/dev/tankpillow/cosmicraft/CosmicraftDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.tankpillow.cosmicraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CosmicraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
